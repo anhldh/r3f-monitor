d312e03a3bbcb29964c582b223d42fc76cbd5307
Lê Đình Hoàng Anh <ledinhhoanganh10@gmail.com>
2026-07-15 14:01:07 +0700

    feat: update package && format code

--- src/components/PerfHeadless.tsx
+++ src/components/PerfHeadless.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import { type FC, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 
--- src/internal.ts
+++ src/internal.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MathUtils } from "three";
 
 declare global {
